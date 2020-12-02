65902d42269bafe97149e1495e5348b084acf810
get-got <43052079+get-got@users.noreply.github.com>
2020-12-02 00:18:14 -0800

    version bump

--- vars.go
+++ vars.go
@@ -5,7 +5,7 @@ import "github.com/fatih/color"
 const (
 	projectName    = "discord-downloader-go"
 	projectLabel   = "Discord Download Bot"
-	projectVersion = "1.3.3"
+	projectVersion = "1.3.4"
 	projectIcon    = "https://discordguide.github.io/assets/Gopher.png"
 
 	projectRepoURL       = "https://github.com/get-got/discord-downloader-go"
