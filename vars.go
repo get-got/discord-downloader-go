5f4332a792d9c3a46cefc90511f2dec7f9d7b810
Drn <43052079+get-got@users.noreply.github.com>
2021-03-15 01:28:49 -0700

    release version

--- vars.go
+++ vars.go
@@ -9,7 +9,7 @@ import (
 const (
 	projectName    = "discord-downloader-go"
 	projectLabel   = "Discord Downloader"
-	projectVersion = "1.4.2-a"
+	projectVersion = "1.4.2"
 	projectIcon    = "https://discordguide.github.io/assets/Gopher.png"
 
 	projectRepoURL       = "https://github.com/get-got/discord-downloader-go"
