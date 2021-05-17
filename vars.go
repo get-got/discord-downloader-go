b2bd11c64d7af3425369a7aabfd9cbe73f0f09d8
Drn <43052079+get-got@users.noreply.github.com>
2021-05-16 21:19:44 -0700

    dev version bump

--- vars.go
+++ vars.go
@@ -9,7 +9,7 @@ import (
 const (
 	projectName    = "discord-downloader-go"
 	projectLabel   = "Discord Downloader"
-	projectVersion = "1.5.3"
+	projectVersion = "1.5.4-dev"
 	projectIcon    = "https://discordguide.github.io/assets/Gopher.png"
 
 	projectRepo          = "get-got/discord-downloader-go"
