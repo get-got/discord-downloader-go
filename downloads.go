5d6201ed19d563f49924b44ca9c5c63f643de5e0
Drn <43052079+get-got@users.noreply.github.com>
2022-06-28 14:48:12 -0700

    c.vxtwitter.com

--- downloads.go
+++ downloads.go
@@ -207,6 +207,7 @@ func getDownloadLinks(inputURL string, channelID string) map[string]string {
 	inputURL = strings.ReplaceAll(inputURL, "mobile.twitter", "twitter")
 	inputURL = strings.ReplaceAll(inputURL, "fxtwitter.com", "twitter.com")
 	inputURL = strings.ReplaceAll(inputURL, "vxtwitter.com", "twitter.com")
+	inputURL = strings.ReplaceAll(inputURL, "c.vxtwitter.com", "twitter.com")
 
 	if regexUrlTwitter.MatchString(inputURL) {
 		links, err := getTwitterUrls(inputURL)
